5f89f8eadf28302a42b60050443f558f05e7191f
s-s-h <sims6625@gmail.com>
2023-02-13 09:14:12 +0900

    테스트 9:14

---
+++ js/main.js
@@ -0,0 +1,13 @@
+<form class="LOGIN" action="/login">
+    <div class="login_id">
+        <i class="material-icons prefix">account_circle</i>ID
+        <input type="text" name="id" id="id" placeholder="ID">
+    </div>
+    <div class="">
+        <h4><i class="material-icons prefix">vpn_key</i>Password</h4>
+        <input type="password" name="PASSWORD" id="pw">
+    </div>
+    <div class="submit">
+        <input type="submit" value="submit">
+    </div>
+</form>
