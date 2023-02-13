9e2d0ca1697d9b9a087c479ea9dbbc20217a964e
s-s-h <sims6625@gmail.com>
2023-02-13 09:46:37 +0900

    Co-authored-by: Geonho <GeonhoGit@users.noreply.github.com>

--- js/main.js
+++ js/main.js
@@ -1,13 +1,24 @@
-<form class="LOGIN" action="/login">
-    <div class="login_id">
-        <i class="material-icons prefix">account_circle</i>ID
-        <input type="text" name="id" id="id" placeholder="ID">
-    </div>
-    <div class="">
-        <h4><i class="material-icons prefix">vpn_key</i>Password</h4>
-        <input type="password" name="PASSWORD" id="pw">
-    </div>
-    <div class="submit">
-        <input type="submit" value="submit">
-    </div>
-</form>
+let id = "sim"
+let password = "1234"
+
+$("#asd").on("click",function(){
+    var id_id = $("#input_id").val()
+    var password_pass = $("#input_password").val()
+    if(id_id == id){
+        if(password_pass == password){
+            location.href("https://www.naver.com/")
+            console.log(id_id)
+        }else{ alert("올바른 아이디를 입력해주시기 바랍니다.")}
+    }else{ alert("올바른 비밀번호를 입력해주시기 바랍니다.")}
+})
+
+function concet(){
+    var id_id = document.getElementById("input_id").value;
+    var password_pass = document.getElementById("input_password").value;
+    if(id_id == id){
+        if(password_pass == password){
+            location.href = "https://www.naver.com/"
+            console.log(id_id)
+        }else{alert("올바른 비밀번호를 입력해주시기 바랍니다.") }
+    }else{ alert("올바른 아이디를 입력해주시기 바랍니다.")}
+}
